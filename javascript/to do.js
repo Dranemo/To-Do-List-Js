3814c49a6d818c3a334fa1d7c96735f4968c3ad5
unknown <ycaillot@gamingcampus.local>
2022-10-18 11:55:10 +0200

    enregistré

--- javascript/to do.js
+++ javascript/to do.js
@@ -1,27 +1,10 @@
-var button = document.getElementsByClassName('btn_s');
+const input = document.querySelector(".input");
+const button = document.querySelector(".button");
+const list = document.querySelector(".list");
 
-function myFunction() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementsByClassName("btn_s").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("Ecrivez quelque chose!");
-    } else {
-      document.getElementsByClassName("list").appendChild(li);
-    }
-    document.getElementsByClassName("btn_s").value = "";
-  
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
-    }
-  }
+button.addEventListener("click", add);
+
+function add(event) {
+    event.preventDefault();
+    const div = document.createElement("div");
+}
