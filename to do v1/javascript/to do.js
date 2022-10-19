fd3408159ebc92fe74fe69ceedd542a13c9452be
unknown <ycaillot@gamingcampus.local>
2022-10-19 10:31:43 +0200

    to do grab

--- to do v1/javascript/to do.js
+++ to do v1/javascript/to do.js
@@ -1,9 +1,63 @@
-let input = document.getElementsByClassName(".input").value;
+// code pour créer de nouvelles tâches
 
-function GetValue() {
-  console.log(input);
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
 
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementsByClassName("input").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("input").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+const filterOption = document.querySelector(".filter-todo");
+filterOption.addEventListener("input", filterTodo);
 
 
 
@@ -15,6 +69,8 @@ function GetValue() {
 
 
 
+
+// code pour les colonnes et le drag and drop
   const items = document.querySelectorAll(".item");
 const columns = document.querySelectorAll(".column");
 let currentItem = null; // Перетаскиваемый элемент
@@ -108,3 +164,6 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+
+
