45dd8f9825cf5fed2ce89015f7838d2357fee99f
unknown <ycaillot@gamingcampus.local>
2022-10-19 09:24:11 +0200

    to do faites

---
+++ to do list deja faites/drag and drop/js.js
@@ -0,0 +1,74 @@
+const items = document.querySelectorAll(".item");
+const columns = document.querySelectorAll(".column");
+let currentItem = null; // Перетаскиваемый элемент
+let nextItem = null; // Перетаскиваемый элемент
+
+const reCalc = () => {
+  columns.forEach((column) => {
+    const childs = [...column.children].filter((child) => {
+      return child.classList.contains("item");
+    });
+    column.querySelector(".cnt").innerHTML = String(childs.length);
+  });
+};
+reCalc();
+
+const onDragStart = (e) => {
+  currentItem = e.target;
+  e.target.classList.add("hold");
+  setTimeout(() => {
+    e.target.classList.add("hide");
+  }, 0);
+};
+
+const onDragEnd = (e) => {
+  e.target.className = "item";
+};
+
+const onDragOver = (e) => {
+  e.preventDefault();
+  const currentElement = e.target;
+  nextItem =
+    currentElement === currentItem
+      ? currentElement.nextElementSibling
+      : currentElement;
+  if (nextItem?.classList.contains("item")) {
+    e.currentTarget.insertBefore(currentItem, nextItem);
+  } else {
+    e.currentTarget.append(currentItem);
+  }
+};
+
+const onDragEnter = (e) => {
+  e.currentTarget.classList.add("entered");
+  reCalc();
+};
+
+const onDragLeave = (e) => {};
+
+const onDrop = (e) => {
+  e.currentTarget.classList.remove("entered");
+  reCalc();
+};
+
+const clearBorders = (e) => {
+  if (
+    !e.target.classList.contains("column") &&
+    !e.target.parentNode.classList.contains("column")
+  ) {
+    columns.forEach((column) => column.classList.remove("entered"));
+  }
+};
+
+items.forEach((item) => {
+  item.addEventListener("dragstart", onDragStart);
+  item.addEventListener("dragend", onDragEnd);
+});
+
+columns.forEach((column) => {
+  column.addEventListener("dragover", onDragOver),
+    column.addEventListener("dragenter", onDragEnter),
+    column.addEventListener("drop", onDrop);
+});
+
+document.addEventListener("dragenter", clearBorders);
